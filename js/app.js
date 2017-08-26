03a93276c13e675ae2b7fe920b990cacfcb211f1
edward-ly <edvard.ly@gmail.com>
2017-08-26 13:03:45 -0700

    Make current tab active again.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 $(document).ready(() => {
   // Load components
-   $("#footer").load("components/footer.html");
+  $("#footer").load("components/footer.html");
+  $("#navbar").load("components/navbar.html", () => {
 
-   // Make navbar tab on current page active
-   var page_id = $("#my-script").attr("page-id");
+    // Make navbar tab on current page active
+    var page_id = $("#my-script").attr("page-id");
+    var current_tab = $("#bs-example-navbar-collapse-1 li").eq(page_id);
+
+    current_tab.addClass("active");
+    current_tab.find("a").append("<span class=\"sr-only\">(current)</span>");
+  });
 });
