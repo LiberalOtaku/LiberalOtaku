ea574119b109e2d5ce9a94ee4b395d51bacc2cdc
edward-ly <edvard.ly@gmail.com>
2017-08-26 11:20:46 -0700

    Add argument to script.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(() => {
+  // Load components
    $("#footer").load("components/footer.html");
+
+   // Make navbar tab on current page active
+   var page_id = $("#my-script").attr("page-id");
 });
